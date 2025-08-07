9eb05e8c69fc2bbf08f2aa7c4e2e082a3f1e133c
MrRiver69 <riversbenjamin5@gmail.com>
2025-08-07 12:51:34 +1000

    feature project update

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       longDescription: "Sustainable Shores is an open-world ocean exploration game where player, after getting lost at sea, finds an island with a starving population and must learn how to sustainable fish the area to prevent the island from starving.",
       technologies: ["Unity", "C#", "Blender"],
       role: "Lead 3D Modeler",
-      thumbnailImage: "/Images/Models/Boat/BoatTextured.JPG",
+      thumbnailImage: "/Images/Games/Fish/Fish6.JPG",
       features: [
         "Procedurally generated ocean wave system",
         "Dynamic day and night cycle",
@@ -26,11 +26,12 @@ export default function Home() {
         "Fishing mini-game mechanic"
       ],
       images: [
-        { src: "/Images/Models/Boat/BoatTextured.JPG", alt: "Boat Model - Exterior hull view" },
-        { src: "/Images/Models/Boat/BoatUntextured.JPG", alt: "Boat Model - Cockpit interior" },
-        { src: "/Images/Models/Boat/BoatWireframe.JPG", alt: "Boat Model - Engine detail" },
-        { src: "/Images/Models/Boat/BoatWireframe.JPG", alt: "Boat Model - Engine detail" },
-        { src: "/Images/Models/Boat/BoatWireframe2.JPG", alt: "Boat Model - Wireframe view" }
+        { src: "/Images/Games/Fish/Fish6.JPG", alt: "Sustainable Shores - Character customization interface" },
+        { src: "/Images/Games/Fish/Fish1.JPG", alt: "Sustainable Shores - Spacecraft approaching a ringed planet" },
+        { src: "/Images/Games/Fish/Fish2.JPG", alt: "Sustainable Shores - Space station exterior" },
+        { src: "/Images/Games/Fish/Fish3.JPG", alt: "Sustainable Shores - Alien landscape with unique flora" },
+        { src: "/Images/Games/Fish/Fish4.JPG", alt: "Sustainable Shores - Trading interface and economy screen" },
+        { src: "/Images/Games/Fish/Fish5.JPG", alt: "Sustainable Shores - Character customization interface" }
       ],
       type: "game",
     },
@@ -40,7 +41,7 @@ export default function Home() {
       longDescription: "Cyber Ninja is a game about a ninja from the future who has the abilty to hack and take control of robotic devices.",
       technologies: ["Unity", "C#", "Blender"],
       role: "3D Artist and Level Designer",
-      thumbnailImage: "/Images/Models/Boat/BoatTextured.JPG",
+      thumbnailImage: "Images/Games/Cyber/Cyber1.JPG",
       features: [
         "Fast paced puzzle platformer",
         "Double jump",
@@ -49,11 +50,12 @@ export default function Home() {
         "Unique visuals and stylization"
       ],
       images: [
-        { src: "/Images/Models/Boat/BoatTextured.JPG", alt: "Boat Model - Exterior hull view" },
-        { src: "/Images/Models/Boat/BoatUntextured.JPG", alt: "Boat Model - Cockpit interior" },
-        { src: "/Images/Models/Boat/BoatWireframe.JPG", alt: "Boat Model - Engine detail" },
-        { src: "/Images/Models/Boat/BoatWireframe.JPG", alt: "Boat Model - Engine detail" },
-        { src: "/Images/Models/Boat/BoatWireframe2.JPG", alt: "Boat Model - Wireframe view" }
+        { src: "Images/Games/Cyber/Cyber1.JPG", alt: "Cyber Ninja - Character customization screen" },
+        { src: "Images/Games/Cyber/Cyber2.JPG", alt: "Cyber Ninja - Ancient temple dungeon" },
+        { src: "Images/Games/Cyber/Cyber3.JPG", alt: "Cyber Ninja - Combat with a dragon" },
+        { src: "Images/Games/Cyber/Cyber4.JPG", alt: "Cyber Ninja - Enchanted forest environment" },
+        { src: "Images/Games/Cyber/Cyber5.JPG", alt: "Cyber Ninja - Inventory and crafting system" },
+        { src: "Images/Games/Cyber/Cyber6.JPG", alt: "Cyber Ninja - Inventory and crafting system" }
       ],
       type: "game",
     },
@@ -63,7 +65,7 @@ export default function Home() {
       longDescription: "Door Horror is a short horror game where the player must solve a series of puzzles to unlock various doors to find the exit.",
       technologies: ["Unity", "C#", "Blender", "Substance Painter"],
       role: "Lead Programmer and Game Designer",
-      thumbnailImage: "/Images/Models/Boat/BoatTextured.JPG",
+      thumbnailImage: "/Images/Games/Door/Door5.JPG",
       features: [
         "Features four unique puzzles for the player to solve",
         "Unique stylized model textures",
@@ -72,13 +74,87 @@ export default function Home() {
         "Minimalistic UI and HUD"
       ],
       images: [
-        { src: "/Images/Models/Boat/BoatTextured.JPG", alt: "Boat Model - Exterior hull view" },
-        { src: "/Images/Models/Boat/BoatUntextured.JPG", alt: "Boat Model - Cockpit interior" },
-        { src: "/Images/Models/Boat/BoatWireframe.JPG", alt: "Boat Model - Engine detail" },
-        { src: "/Images/Models/Boat/BoatWireframe.JPG", alt: "Boat Model - Engine detail" },
-        { src: "/Images/Models/Boat/BoatWireframe2.JPG", alt: "Boat Model - Wireframe view" }
+        { src: "/Images/Games/Door/Door1.JPG", alt: "Door Horror - Neon-lit cityscape view" },
+        { src: "/Images/Games/Door/Door2.JPG", alt: "Door Horror - Character dialogue system" },
+        { src: "/Images/Games/Door/Door3.JPG", alt: "Door Horror - Evidence board investigation" },
+        { src: "/Images/Games/Door/Door4.JPG", alt: "Door Horror - Abandoned subway environment" },
+        { src: "/Images/Games/Door/Door5.JPG", alt: "Door Horror - Night club scene with unique lighting" },
+        { src: "/Images/Games/Door/Door6.JPG", alt: "Door Horror - Night club scene with unique lighting" }
       ],
       type: "game",
+    },
+    {
+      title: "A Space Moment",
+      description: "A short space experience.",
+      longDescription: "A space moment is a short expeirence that aims to capture the feeling of being in space within the player.",
+      technologies: ["Unity", "C#", "Blender"],
+      role: "Solo Developer",
+      thumbnailImage: "/Images/Games/Space/Space1.JPG",
+      features: [
+        "A space atmospheric feeling",
+        "Simplistic game mechanics",
+        "Minimalistic UI and HUD",
+        "Delicious audio",
+        "Variable gravity"
+      ],
+      images: [
+        { src: "/Images/Games/Space/Space1.JPG", alt: "A Space Moment - Neon-lit cityscape view" },
+        { src: "/Images/Games/Space/Space2.JPG", alt: "A Space Moment - Character dialogue system" },
+        { src: "/Images/Games/Space/Space3.JPG", alt: "A Space Moment - Evidence board investigation" },
+        { src: "/Images/Games/Space/Space4.JPG", alt: "A Space Moment - Abandoned subway environment" },
+        { src: "/Images/Games/Space/Space5.JPG", alt: "A Space Moment - Night club scene with unique lighting" },
+        { src: "/Images/Games/Space/Space6.JPG", alt: "A Space Moment - Night club scene with unique lighting" }
+      ],
+      type: "game"
+    },
+    {
+      title: "Lagoon Experience",
+      description: "A short water simulation experience.",
+      longDescription: "The lagoon experience is a short experience that aims to capture the feeling of being in the Brisbane Southbank lagoon, with three unique interactions for the player to experience.",
+      technologies: ["UE5", "Blueprint"],
+      role: "Solo Developer",
+      thumbnailImage: "/Images/Games/Lagoon/Lagoon2.JPG",
+      features: [
+        "Virtual Reality experience",
+        "Advanced water simulation interactions",
+        "Three interactive experiences",
+        "Minimalistic UI and HUD",
+        "Rubber Ducks"
+      ],
+      images: [
+        { src: "/Images/Games/Lagoon/Lagoon1.JPG", alt: "Lagoon Experience - Neon-lit cityscape view" },
+        { src: "/Images/Games/Lagoon/Lagoon2.JPG", alt: "Lagoon Experience - Character dialogue system" },
+        { src: "/Images/Games/Lagoon/Lagoon3.JPG", alt: "Lagoon Experience - Evidence board investigation" },
+        { src: "/Images/Games/Lagoon/Lagoon4.JPG", alt: "Lagoon Experience - Abandoned subway environment" },
+        { src: "/Images/Games/Lagoon/Lagoon5.JPG", alt: "Lagoon Experience - Night club scene with unique lighting" },
+        { src: "/Images/Games/Lagoon/Lagoon6.JPG", alt: "Lagoon Experience - Night club scene with unique lighting" }
+      ],
+      type: "game"
+    },
+    {
+      title: "Mower Madness",
+      description: "A Loacal Mower Party Game.",
+      longDescription: "Mower Madness is a local multiplayer game where players compete to cut the most grass in a limited time.",
+      technologies: ["UE5", "Blueprint"],
+      role: "Solo Developer",
+      thumbnailImage: "/Images/Games/Mow/Mow1.JPG",
+      features: [
+        "Fast paced local multiplayer game",
+        "Simplistic game mechanics",
+        "Siza & speed power ups",
+        "Minimalistic UI and HUD",
+        "Player leaderboard"
+      ],
+      images: [
+        { src: "/Images/Games/Mow/Mow6.JPG", alt: "Lagoon Experience - Night club scene with unique lighting" },
+        { src: "/Images/Games/Mow/Mow1.JPG", alt: "Lagoon Experience - Neon-lit cityscape view" },
+        { src: "/Images/Games/Mow/Mow2.JPG", alt: "Lagoon Experience - Character dialogue system" },
+        { src: "/Images/Games/Mow/Mow3.JPG", alt: "Lagoon Experience - Evidence board investigation" },
+        { src: "/Images/Games/Mow/Mow4.JPG", alt: "Lagoon Experience - Abandoned subway environment" },
+        { src: "/Images/Games/Mow/Mow5.JPG", alt: "Lagoon Experience - Night club scene with unique lighting" }
+
+      ],
+      type: "game"
     }
   ];
 
